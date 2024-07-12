085843c7b3ea690998268e78667a4172183b2dee
Xraww <xrw3008@gmail.com>
2024-07-12 06:22:17 +0200

    Adding modifyBook & deleteBook. Multer modification & removing eslint

--- Backend/middlewares/multer-config.js
+++ Backend/middlewares/multer-config.js
@@ -11,9 +11,10 @@ const storage = multer.diskStorage({
         callback(null, "images")
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(" ").join("_");
+        const originalName = file.originalname.split(" ").join("_");
+        const nameWithoutExtension = originalName.substring(0, originalName.lastIndexOf('.'));
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + "." + extension);
+        callback(null, nameWithoutExtension + "_" + Date.now() + "." + extension);
     }
 });
 
